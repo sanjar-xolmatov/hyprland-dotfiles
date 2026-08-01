6cb8b22b81a94afb9fc51546e7d98b0cf8bbcfd8
Sanjar Xolmatov <sanjarxolmatov@proton.me>
2026-08-01 11:03:56 +0500

    Added alias

--- fish/config.fish
+++ fish/config.fish
@@ -9,6 +9,11 @@ alias hx="helix"
 alias vim="nvim"
 alias z="zoxide"
 alias fastfetch="fastfetch --config hypr"
+alias yt-music='yt-dlp -x --audio-format mp3 --embed-metadata --embed-thumbnail --paths "~/Music"'
 
 starship init fish | source
 
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/sanjar/.local/bin" $PATH
